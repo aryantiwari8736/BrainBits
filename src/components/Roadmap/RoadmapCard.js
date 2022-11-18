cbe4a1514d0448b12bee936ea4b9687e9cd74770
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-11-18 18:43:22 +0530

    team page updates

--- src/components/Roadmap/RoadmapCard.js
+++ src/components/Roadmap/RoadmapCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 function RoadmapCard(props) {
   return (
     
-        <div className="px-3 my-4  md:w-1/3 ">
+        <div className="p-4 md:w-1/3 ">
           <a target="_blanck" href={props.link}>
-        <div className="h-full backgroundcard  border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={props.rcardimg} alt="blog"/>
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img  className="lg:h-48 md:h-36 w-full object-cover object-center" src={props.rcardimg} alt="blog"/>
           <div className="p-6">
             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{props.category}</h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{props.heading}</h1>
