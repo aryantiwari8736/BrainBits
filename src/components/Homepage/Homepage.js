63bd2d6ab4751fbafdfccba210f6ee94bb80e21a
Saket <sakettiwari076@gmail.com>
2022-11-18 05:43:02 -0800

    changes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,16 +7,16 @@ const Homepage = () => {
   return (
     <div>
       <Navbar/>
-<section className="text-gray-600  sm:pt-6 sm:mx-10 leading-3  lg:px-32 mainfont body-font">
+<section className="  sm:pt-6 sm:mx-10 leading-3  lg:px-32  body-font">
     {/* guhihi */}
   <div className="container mt-10 mx-auto flex px-5  md:flex-row flex-col items-center">
     <div className="lg:flex-grow  lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left  md:mb-0 items-center text-center">
-      <h1 className="title-font  text-2xl mb font-medium  mainfont letterspacemy"><span className="frontheadcol">BrainBits </span> A Community with Competetive Programers , Leetcoders and Developers.<br></br> <span className='font-bold'>KIET Group of Institutions</span> 
+      <h1 className="title-font text-2xl letterspacemy  mb headingfont letterspacemy "><span className="frontheadcol text-2xl">BrainBits </span>A Community with Competetive  Programers , Developers and Mentors .<br></br> <span className='font-bold'>KIET Group of Institutions</span> 
         {/* <br className="hidden lg:inline-block">readymade gluten</br> */}
       </h1>
       {/* <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p> */}
       
-      <div className="flex justify-center">
+      <div className="flex justify-center mainfont">
         <a href='https://forms.gle/cjCWWXFrfLhUq7oD9'>
         <button className="  inline-flex   font-bold text-white  bg-violet-500 border-0 py-2 px-6 focus:outline-none mt-5 hover:bg-indigo-600 rounded text-lg">Join our community</button>
         </a>
