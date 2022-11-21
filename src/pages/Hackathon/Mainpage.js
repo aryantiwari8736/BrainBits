35a82da044238c1629fa5bee5b91a137e921e409
Saket <sakettiwari076@gmail.com>
2022-11-21 09:11:05 -0800

    s

--- src/pages/Hackathon/Mainpage.js
+++ src/pages/Hackathon/Mainpage.js
@@ -18,7 +18,7 @@ const Mainpage = () => {
     <div className='grid pt-10 mainfont lg:grid-cols-2 mx-10'>
     <div className='justify-center pt-8 lg:pl-32'>
         <h1 className='text-3xl  dark:text-white headingfont text-white  font-semibold '>About us</h1>
-        <p className='text-white dark:text-white pt-5 lg:text-xl headingfont  letterspace  font-normal'>Great ideas can come from anyone, anywhere. When tech enthusiasts come together from different regions, with diverse backgrounds and skill levels, amazing things can happen. These brilliant minds will invent something new – something that could change our daily lives, the way we do business or approach humanitarian causes. Because when curious minds collaborate, the world wins.
+        <p className='text-white dark:text-white pt-5 lg:text-xl headingfont  letterspace  font-normal'>Great ideas can come from anyone, anywhere. When tech enthusiasts come together from different regions, with diverse backgrounds and skill levels, amazing things can happen. These brilliant minds will invent something new something that could change our daily lives, the way we do business or approach humanitarian causes. Because when curious minds collaborate, the world wins.
 
 </p>
 {/* <p className='dark:text-white'>Our members active on </p> */}
@@ -61,7 +61,7 @@ const Mainpage = () => {
 <div className='py-5'>
 <img src='idea.webp' className='h-56  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 relative left-8   duration-300 ...' alt=''/>
 <div className=''>
-    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Open Theme</p>
+    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Open Theddme</p>
 
 </div>
 </div>
