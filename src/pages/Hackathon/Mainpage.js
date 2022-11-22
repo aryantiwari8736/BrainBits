1a5ea4d537a3a229e535c47fb39ed810ec2a25f9
Saket <sakettiwari076@gmail.com>
2022-11-22 01:27:34 -0800

    hackathon:

--- src/pages/Hackathon/Mainpage.js
+++ src/pages/Hackathon/Mainpage.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
 
-const Mainpage = () => {
+const Mainpage = (props) => {
   return (
-    <div className=' bg-gray-900 pb-96 '>
-    <div className='bg-gray-900'>
+    <div className=' bg-gray-900   pb-96 '>
+    <div className='brightness-125'>
     <div className=' fontour h-screen w-screen '>
     
     <div className='grid '>
     <div className='text-white grid   pt-12 cssmobile '>
     
-      < h1 className='  letterspace text-center lg:text-6xl  text-5xl headingfont  headinghgt frontheadcol fontour font-extrabold lg:pl-16 md:pl-16 space-y-4'><span className='border-b-2 frontheadcol textshadow '>Hackathon 1.0</span>  <br></br><span className='red relative top-4 frontheadcol' >BrainBits </span> </h1>
+      < h1 className='  letterspace text-center lg:text-6xl  text-5xl headingfont  headinghgt frontheadcol fontour font-extrabold lg:pl-16 md:pl-16 space-y-4'><span className='border-b-2 frontheadcol textshadow shadow-purple-500/50'>Hackathon 1.0</span>  <br></br><span className='red relative top-4 frontheadcol' >BrainBits </span> </h1>
     </div>
     
     
     </div>
     <div className='grid pt-10 mainfont lg:grid-cols-2 mx-10'>
     <div className='justify-center pt-8 lg:pl-32'>
-        <h1 className='text-3xl  dark:text-white headingfont text-white  font-semibold '>About us</h1>
+        <h1 className='text-3xl  dark:text-white headingfont text-white  font-semibold '>About Hackathon </h1>
         <p className='text-white dark:text-white pt-5 lg:text-xl headingfont  letterspace  font-normal'>Great ideas can come from anyone, anywhere. When tech enthusiasts come together from different regions, with diverse backgrounds and skill levels, amazing things can happen. These brilliant minds will invent something new something that could change our daily lives, the way we do business or approach humanitarian causes. Because when curious minds collaborate, the world wins.
 
 </p>
+<button type="button" class=" my-4 headingfont text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80  rounded-lg  px-5 py-2.5 text-center font- text-xl mr-2 mb-2">Register</button>
+
 {/* <p className='dark:text-white'>Our members active on </p> */}
 
     </div>
@@ -30,9 +32,10 @@ const Mainpage = () => {
 
     </div>
 
-    
-  <div className='text-white  headingfont bg-gray-900 pt-6 '>
-    <h1 className='text-center headingfont py-6  text-3xl text-white'>Domains</h1>
+   {/* Domains starts here  */}
+
+  <div className='text-white brightness-125  headingfont bg-gray-900 pt-6 '>
+    <h1 className='text-center headingfont py-6  text-3xl  frontheadcol lg:text-5xl'>Domains</h1>
 <div className='grid bg-gray-900 justify-center lg:grid-cols-3'>
     <div className='py-5'>
 <img src='webdev.webp' className='h-56 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...' alt=''/>
@@ -59,20 +62,98 @@ const Mainpage = () => {
 </div>
 
 <div className='py-5'>
-<img src='idea.webp' className='h-56  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 relative left-8   duration-300 ...' alt=''/>
+<img src='blockchain.webp' className='h-56  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 relative left-8   duration-300 ...' alt=''/>
 <div className=''>
-    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Open Theddme</p>
+    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Web 3.0</p>
+
+</div>
+</div>
+
+<div className='py-5'>
+<img src='aiml.webp' className='h-56  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 relative left-8   duration-300 ...' alt=''/>
+<div className=''>
+    <p className='text-2xl text-center py-4 backgroundcard mx-4'>AI/ML</p>
+
+</div>
+</div>
+</div>
+</div>
+{/* Complete section ends domain */}
+
+  
+    
+  <div className='text-white  headingfont  bg-gray-900 pt-6 '>
+    <h1 className='text-center headingfont py-6  text-3xl text-white lg:text-5xl frontheadcol'>Why you must take part ?</h1>
+<div className='grid bg-gray-900 mx-8 card justify-center lg:grid-cols-3'>
+    <div className='py-5'>
+<img src='why1.webp' className='h-60 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...' alt=''/>
+<div className=''>
+    <p className='text-2xl text-center py-4  backgroundcard mx-4' >Be a part of our community BrainBits</p>
+
+</div>
+</div>
+
+<div className='py-5'>
+<img src='why1.webp' className='h-56  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...' alt=''/>
+<div className=''>
+    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Interaction with judges</p>
+
+</div>
+</div>
+
+<div className='py-5'>
+<img src='why1.webp' className='h-56 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ... relative left-8' alt=''/>
+<div className=''>
+    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Increase your creativity</p>
+
+</div>
+</div>
+
+<div className='py-5'>
+<img src='why1.webp' className='h-56  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 relative left-8   duration-300 ...' alt=''/>
+<div className=''>
+    <p className='text-2xl text-center py-4 backgroundcard mx-4'>Technical support if you have unique idea</p>
 
 </div>
 </div>
 
 </div>
+</div>
+{/* HHHH*/}
+
+{/* prizes  */}
+<h2 className='text-center text-3xl bg-gray-900 lg:text-5xl brightness-125 text-white  headingfont'>Hackathon Prizes</h2>
+<div className='grid bg-gray-900 brightness-125 lg:grid-cols-2'>
+<div className=" mx-12   p-10 ">
+    <div className="border grid border-x-4 border-y-4 justify-center  backgroundcard  p-14 rounded-lg   ">
+        <img alt='domain' src='money.webp' className='h-9'/>
+    
+      <h2 className=" text-white text-4xl text-center headingfont   font-bold title-font mb-2 mt-2 antialiased ...  lg:hover:text-gray-700 cursor-pointer  ">Winner</h2>
+        <p className="leading-relaxed  text-center  font-extrabold mainfont cursor-pointer text-white text-2xl">Worth Rs 5000</p>
+    </div>
+  </div>
+  <div className=" mx-12   p-10 ">
+    <div className="border grid border-x-4 border-y-4 justify-center  backgroundcard  p-14 rounded-lg   ">
+        <img alt='domain' src='money.webp' className='h-9'/>
+    
+      <h2 className=" text-white text-4xl text-center headingfont   font-bold title-font mb-2 mt-2 antialiased ...  lg:hover:text-gray-700 cursor-pointer  ">Runner Up</h2>
+        <p className="leading-relaxed  text-center  font-extrabold mainfont text-white cursor-pointer text-2xl">Worth Rs 5000</p>
+    </div>
+  </div>
+
+  
+  
+  
+
+  
+
+
   </div>
     </div>
-    
+ </div>
  </div>
     
-         </div>
+         
 
   )
 }
